da2aff38a1a5015b0002c37219d96785e3c35a81
Vergil73 <jl6892139@gmail.com>
2025-09-16 14:43:39 +0545

    promting user for typing a password is not working

--- public/terminal.js
+++ public/terminal.js
@@ -3,42 +3,59 @@
 // const terminalContainer = document.querySelector(".terminal-container");
 const terminalInput = document.querySelector("#terminal-input");
 const terminalOutput = document.querySelector(".terminal-output");
-// const terminalResult = document.querySelector("#terminal-result");
 
-
+// Global variables
+let waitingForPassword = false;
+let currentUsername = null;
 
 function clear(){
     terminalOutput.innerHTML = "";
 }
 
+function createDate(){
+    const date = new Date();
+    let paragraph = document.createElement('p');
+    paragraph.textContent = new Date()
+    terminalOutput.append(paragraph);
+}
+
+function newPassword(){
+    const terminalValue = terminalInput.value;
+    terminalInput.value = "";
+
+    const passwdHead = document.createElement("p");
+    passwdHead.className = "input";
+    passwdHead.textContent = `password for ${currentUsername}: ` + terminalValue;
+    terminalOutput.append(passwdHead);    
+}
+
 function newUser(args){
     if(!args || args.length === 0){
-        let add = document.createElement('p');
-        add.textContent = "invalid username";
-        terminalOutput.append(add);
+        const paragraph = document.createElement('p');
+        paragraph.textContent = "invalid username";
+        terminalOutput.append(paragraph);
         console.log("invalid username");
     } else{
-        console.log(`new user: ${args}`);
-        let add = document.createElement('p');
-        add.textContent = `new user: ${args[0]}`;
-        terminalOutput.append(add);
-    }
+        waitingForPassword = true;
+        currentUsername = args[0];
+      // console.log(`new user: ${username}`);
+    }  
+       
 }
 
 const commands = {
     whoami: "I am Vergil.",
     ls: "currently empty",
     touch: " creates new file",
-    help: " List of commands available: whoami, ls, touch ",
+    date: createDate,
+    help: " List of commands available: whoami, ls, touch, date ",
     clear: clear,
-    // user: getUser,
     useradd : newUser
 };
 
 // For head of terminal like usr@local$- 
 const head = document.querySelector("#head");
-head.textContent = "usr@local ~ $ ";
-
+head.textContent = `usr@local ~ $ `;
 
 function commandProcess(cmd){
     // Takes the argument from the user with the commands/key and values
@@ -47,10 +64,10 @@ function commandProcess(cmd){
     const args = parts.slice(1); 
     let userInput = commands[command];
 
+
     if (typeof userInput === "function" ) {
         userInput(args); // function commands run first
     } 
-    
     else if (userInput) {
         // handle string responses
         let cmdResult = document.createElement("p");
@@ -70,14 +87,19 @@ terminalInput.addEventListener("keydown", function(e) {
         const terminalValue = terminalInput.value;
         terminalInput.value = "";
 
-
         const inputHead = document.createElement("p");
         inputHead.className = "input";
         inputHead.textContent = head.textContent + terminalValue;
         terminalOutput.append(inputHead);
 
+        if(waitingForPassword){
+            newPassword();
+            console.log("waiting for password: ",waitingForPassword);
+            waitingForPassword = false;
+        } else {
         commandProcess(terminalValue);
-    }
+            console.log("Normal commands");
+        }
+
+    } 
 })
-
-
